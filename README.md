69abdfb5a9d518424630e55dd45ff60f8fe937ca
Dmitry Dolgushin <razor9714@gmail.com>
2019-08-03 16:10:12 +0500

    completed the fifth step

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/09e9bcb6114e8e3cf851/maintainability)](https://codeclimate.com/github/DOMININ/frontend-project-lvl1/maintainability)
 
 [![Build Status](https://travis-ci.org/DOMININ/frontend-project-lvl1.svg?branch=master)](https://travis-ci.org/DOMININ/frontend-project-lvl1)
+
+Installing:
+
+<a href="https://asciinema.org/a/kQ4VJzgKvUT9KcMvgzjLD98MG" target="_blank"><img src="https://asciinema.org/a/kQ4VJzgKvUT9KcMvgzjLD98MG" /></a>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import readlineSync from 'readline-sync';
 
-const getName = () => {
+const getEven = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no". \n');
   const name = readlineSync.question('May I have your name? ');
-  return console.log(`Hello, ${name}!`);
+  console.log(`Hello, ${name}! \n`);
+
+  for (let i = 0; i < 3; i += 1) {
+    const number = Math.floor(Math.random() * (20 - 1 + 1)) + 1;
+    const answerCorrect = number % 2 === 0 ? 'yes' : 'no';
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    if (answer === answerCorrect) {
+      console.log('Correct!');
+    } else {
+      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${answerCorrect}'. \nLet's try again, ${name}!`);
+    }
+  }
+  return console.log(`Congratulations, ${name}`);
 };
 
-export default getName;
+export default getEven;
